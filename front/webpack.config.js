2b688779fd61b514dc044b8af37a5f24038f198b
t-kusakabe <ttmac0704@gmail.com>
2018-05-29 01:48:34 +0900

    Add: webpack.config

---
+++ front/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+const config = {
+  entry: __dirname + '/src/app.tsx',
+  output: {
+    path: path.resolve('../app/assets/javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {loader: 'ts-loader'}
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
